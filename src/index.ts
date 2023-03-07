0c3d995b91210006dd88f32698d0c3a829957e34
Marek Zelinka <mzelinka17@gmail.com>
2023-03-07 17:47:56 +0100

    solve ex9.8 - setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("working!");
+import express from "express";
+
+const app = express();
+const port = 3003;
+
+app.get("/ping", (_request, response) => {
+  response.send("pong");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
